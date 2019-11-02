66ba816ec83a22f60233bdea18cdba97359d7f95
vineethsuresh1 <36769737+vineethsuresh1@users.noreply.github.com>
2019-11-01 20:42:59 -0500

    Update CourseArea.js

--- src/Components/CourseArea.js
+++ src/Components/CourseArea.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import Course from './Course';
@@ -25,31 +24,3 @@ class CourseArea extends React.Component {
 }
 
 export default CourseArea;
-=======
-import React from 'react';
-import './App.css';
-import Course from './Course';
-
-class CourseArea extends React.Component {
-  getCourses() {
-    let courses = [];
-
-    for(const course of Object.entries(this.props.data)) {
-      courses.push (
-        <Course key={course[0]} data={course[1]}/>
-      )
-    }
-    return courses;
-  }
-
-  render() {
-    return (
-      <div style={{margin: '5px'}}>
-        {this.getCourses()}
-      </div>
-    )
-  }
-}
-
-export default CourseArea;
->>>>>>> My first commit
